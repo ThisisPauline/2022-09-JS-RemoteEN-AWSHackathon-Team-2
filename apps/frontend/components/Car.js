627a26f3ef907baf5967849d44d0a42e7cf5cdfd
Pierre Basck <pierre.basck@gmail.com>
2023-01-11 18:36:32 +0100

    frontend linked

--- apps/frontend/components/Car.js
+++ apps/frontend/components/Car.js
@@ -3,9 +3,16 @@ const Car = ({ cars }) => {
     <ul>
       {cars.map((cars) => {
         return (
-          <li key={cars.id}>
-            <div>Name: {cars.CarName}</div>
-          <div>PowerSteering? {cars.PowerSteering}</div>
+          <li className="mt-20" key={cars.id}>
+            <div><b>id:</b> {cars.CarId}</div>
+            <div><b>BrandId:</b> {cars.BrandId}</div>
+            <div><b>Name:</b> {cars.CarName}</div>
+            <div><b>CarImage:</b> {cars.CarImage}</div>
+            <div><b>PowerSteering:</b> {cars.PowerSteering}</div>
+            <div><b>TransmissionMode:</b> {cars.TransmissionMode}</div>
+            <div><b>NbDoors:</b> {cars.NbDoors}</div>
+            <div><b>NbPassengers:</b> {cars.NbPassengers}</div>
+            <div><b>DailyPrice:</b> {cars.DailyPrice}</div>
           </li>
         );
       })}
