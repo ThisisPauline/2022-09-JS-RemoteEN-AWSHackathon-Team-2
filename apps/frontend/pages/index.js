38a1694369b158a56a37237c78ff3cdc9e147ef3
Pierre Basck <pierre.basck@gmail.com>
2023-01-11 15:40:50 +0100

    Header

--- apps/frontend/pages/index.js
+++ apps/frontend/pages/index.js
@@ -2,14 +2,14 @@ import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "@next/font/google";
 import styles from "../styles/Home.module.scss";
-import header from "../components/Header"
+import Header from "../components/Header"
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <div className="bg-[#242F3E] h-screen">
-      <header />
+      <Header />
       <Image className="mx-auto mt-10 w-30 h-30" src={awsLogo} />
       <h1 className="flex justify-center mt-5 mb-10 text-white">RENT A CAR</h1>
       <Image className="w-90 h-90" src={carPicture} />
