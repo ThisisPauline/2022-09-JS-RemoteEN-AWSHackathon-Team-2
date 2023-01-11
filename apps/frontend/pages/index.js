c6882826eaca87c865a20a9987cef30507acabd5
Pierre Basck <pierre.basck@gmail.com>
2023-01-11 16:19:00 +0100

    titles dones

--- apps/frontend/pages/index.js
+++ apps/frontend/pages/index.js
@@ -10,14 +10,14 @@ const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <div className="bg-[#242F3E] h-screen">
+    <div>
       <Header />
       <Image className="mx-auto mt-10 w-30 h-30" src={awsLogo} />
       <h1 className="flex justify-center mt-5 mb-10 text-white">RENT A CAR</h1>
       <Image className="w-90 h-90" src={carPicture} />
       <div className="border ml-30">Rent a Car</div>
-      <div className="">Book a car with AWS Rent a Car</div>
-      <div className="">
+      <div className="my-10 text-[#F3971F] text-[32px] font-bold ml-10 mr-10 text-center">Book a car with AWS Rent a Car</div>
+      <div className="mx-10 text-[#F3971F]">
         Welcome to AWS Rent a Car, a global leader in car rental and you can
         hire a car or a commercial van from any one of our 3835 rental locations
         worldwide. Testn2
