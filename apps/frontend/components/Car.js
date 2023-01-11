50885ac15c5809965f90ab669b3f2ad580e99129
Pierre Basck <pierre.basck@gmail.com>
2023-01-11 19:13:50 +0100

    added button on choose car

--- apps/frontend/components/Car.js
+++ apps/frontend/components/Car.js
@@ -4,15 +4,36 @@ const Car = ({ cars }) => {
       {cars.map((cars) => {
         return (
           <li className="mt-20" key={cars.id}>
-            <div><b>id:</b> {cars.CarId}</div>
-            <div><b>BrandId:</b> {cars.BrandId}</div>
-            <div><b>Name:</b> {cars.CarName}</div>
-            <div><b>CarImage:</b> {cars.CarImage}</div>
-            <div><b>PowerSteering:</b> {cars.PowerSteering}</div>
-            <div><b>TransmissionMode:</b> {cars.TransmissionMode}</div>
-            <div><b>NbDoors:</b> {cars.NbDoors}</div>
-            <div><b>NbPassengers:</b> {cars.NbPassengers}</div>
-            <div><b>DailyPrice:</b> {cars.DailyPrice}</div>
+            <div>
+              <b>id:</b> {cars.CarId}
+            </div>
+            <div>
+              <b>BrandId:</b> {cars.BrandId}
+            </div>
+            <div>
+              <b>Name:</b> {cars.CarName}
+            </div>
+            <div>
+              <b>CarImage:</b> {cars.CarImage}
+            </div>
+            <div>
+              <b>PowerSteering:</b> {cars.PowerSteering}
+            </div>
+            <div>
+              <b>TransmissionMode:</b> {cars.TransmissionMode}
+            </div>
+            <div>
+              <b>NbDoors:</b> {cars.NbDoors}
+            </div>
+            <div>
+              <b>NbPassengers:</b> {cars.NbPassengers}
+            </div>
+            <div>
+              <b>DailyPrice:</b> {cars.DailyPrice}
+            </div>
+            <button className="mt-10 bg-[#F3971F] text-white font-normal py-2 px-4 mx-8 rounded-full w-full">
+              Choose Car
+            </button>
           </li>
         );
       })}
