4d6f27c683f35caf94936c48fb241ab49c47540d
Pauline Kraus <kraus.pauline1@gmail.com>
2023-01-12 19:09:15 +0100

    color fix

--- apps/frontend/pages/vehiculesCompany.js
+++ apps/frontend/pages/vehiculesCompany.js
@@ -1,30 +1,30 @@
 import React from "react";
 import { AiOutlinePlusCircle } from "react-icons/ai";
-import close from "../assets/close.svg";
+import closeDark from "../assets/closeDark.svg";
 import Image from "next/image";
 
 const myBookings = () => {
   return (
-    <div className=" bg-[#121A25] h-screen">
+    <div className=" bg-white h-screen">
       <div className="flex flex-row pt-10 mb-[42px] justify-end pr-5 items-end">
         <button>
-          <Image src={close} />
+          <Image src={closeDark} />
         </button>
       </div>
       <div className="flex flex-col justify-center items-center">
         <div>
-          <h1 className="text-3xl font-extrabold text-[#F3971F]">
+          <h1 className="text-3xl font-extrabold text-[#121A25]">
             My Vehicules
           </h1>
         </div>
 
-        <p className="my-40 text-white text-xl uppercase text-center">
+        <p className="my-40 text-[#121A25] text-xl uppercase text-center">
           You don't have any vehicules registered yet
         </p>
 
         <div className="flex flex-col justify-center items-center">
-          <AiOutlinePlusCircle className="text-5xl text-[#F3971F] text-center my-2" />
-          <p className="text-[#F3971F]">Add a vehicule</p>
+          <AiOutlinePlusCircle className="text-5xl text-[#121A25] text-center my-2" />
+          <p className="text-[#121A25]">Add a vehicule</p>
         </div>
       </div>
     </div>
